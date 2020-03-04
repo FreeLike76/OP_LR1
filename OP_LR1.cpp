68ad5965c1eee86f1c8e90f9215bc2a75c4065f1
Dmytro Geleshko <dimahunter17@gmail.com>
2020-03-04 15:30:08 +0100

    Added class Team

--- OP_LR1.cpp
+++ OP_LR1.cpp
@@ -1,11 +1,43 @@
 // OP_LR1
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
+class Team
+{
+	string name;
+	int score;
+public:
+	string getName()
+	{
+		return this->name;
+	}
+	string setName(string a)
+	{
+		this->name = a;
+	}
+	int getScore()
+	{
+		return this->score;
+	}
+	int setScore(int a)
+	{
+		this->score = a;
+	}
+	int addScore(int a)
+	{
+		this->score += a;
+	}
+	Team()
+	{
+		this->name = "Unknown";
+		this->score = 0;
+	}
+};
+
 int main()
 {
-    cout << "Hello World!";
-	cout << "Just Test";
+
 }
