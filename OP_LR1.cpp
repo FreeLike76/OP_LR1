2e9f89e3d7046cf05029e4c776a92da2489cf1bc
Dmytro Geleshko <dimahunter17@gmail.com>
2020-03-05 13:29:24 +0100

    File reading is done

--- OP_LR1.cpp
+++ OP_LR1.cpp
@@ -14,7 +14,7 @@ public:
 	{
 		return this->name;
 	}
-	string setName(string a)
+	void setName(string a)
 	{
 		this->name = a;
 	}
@@ -22,11 +22,11 @@ public:
 	{
 		return this->score;
 	}
-	int setScore(int a)
+	void setScore(int a)
 	{
 		this->score = a;
 	}
-	int addScore(int a)
+	void addScore(int a)
 	{
 		this->score += a;
 	}
@@ -37,7 +37,63 @@ public:
 	}
 };
 
+Team* readfile(string path, int* sizes, int iter);
+
 int main()
 {
+	string path;
+	cout << "Enter number of files: ";
+	int filenum;
+	cin >> filenum;
+	int* sizes = new int[filenum];
+	Team** All = new Team * [filenum];
+	for (int i = 0; i < filenum; i++)
+	{
+		cout << "Enter the name of " << i + 1 << " file: ";
+		cin >> path;
+		All[i] = readfile(path,sizes,i);
+	}
+	cout << All[0][1].getName() << endl << All[0][2].getName() << All[0][2].getScore() << endl;;
+	cout << All[1][2].getName() << All[1][2].getScore();
+}
 
+Team* readfile(string path,int* sizes,int iter)
+{
+	ifstream file;
+	file.open(path);
+	if (!file.is_open())
+		cout << "Cannot open file!";
+	else
+	{
+		char ch;
+		int a, b,size;
+		file >> size;
+		file.get(ch);
+		sizes[iter] = size;
+		Team* thisfileteams = new Team[size];
+		for (int i = 0; i < size; i++)
+		{
+			file.get(ch);
+			string fullname;
+			while (ch != ',')
+			{
+				fullname = fullname + ch;
+				file.get(ch);
+			}
+			thisfileteams[i].setName(fullname);
+			while (ch != '\n'&&!file.eof())
+			{
+				file >> a;
+				file.get(ch);
+				file >> b;
+				if (a > b)
+					thisfileteams[i].addScore(3);
+				else if (a == b)
+					thisfileteams[i].addScore(1);
+				file.get(ch);
+			}
+
+		}
+		return thisfileteams;
+	}
 }
